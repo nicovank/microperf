02b32a0cf27f218c51573351d93d052baa9939df
Nicolas van Kempen <nvankemp@gmail.com>
2023-08-14 14:30:48 -0400

    Add Boost::program_options dependency

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,17 @@
 #include <cstdlib>
+#include <cstring>
 #include <string>
 
 #include <boost/program_options.hpp>
 
 #include <fmt/core.h>
 
-int main() {
+int main(int argc, char** argv) {
     fmt::println("Hello, World!");
+    for (int i = 0; i < argc; ++i) {
+        if (strncmp(argv[i], "---", 3) == 0) {
+            fmt::println("Found overflow arguments...");
+        }
+    }
     return EXIT_SUCCESS;
 }
